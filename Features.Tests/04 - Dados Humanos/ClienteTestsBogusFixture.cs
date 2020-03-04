ad81775d80e0a12eb16e4e2d7249062fe4c26eb9
lucasven <lucas.venturella@hotmail.com>
2020-03-03 21:42:24 -0300

    utilizacao do bogus para geracao de dados fake

---
+++ Features.Tests/04 - Dados Humanos/ClienteTestsBogusFixture.cs
@@ -0,0 +1,59 @@
+﻿using Features.Clientes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Bogus;
+using Bogus.DataSets;
+
+namespace Features.Tests
+{
+    [CollectionDefinition(nameof(ClienteBogusCollection))]
+    public class ClienteBogusCollection : ICollectionFixture<ClienteTestsFixture>
+    { }
+
+    public class ClienteTestsBogusFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            var genero = new Faker().PickRandom<Name.Gender>();
+            var cliente = new Faker<Cliente>("pt_BR")
+                .CustomInstantiator(f => new Cliente(
+                   Guid.NewGuid(),
+                   f.Name.FirstName(genero),
+                   f.Name.LastName(genero),
+                   f.Date.Past(80, DateTime.Now.AddYears(-18)),
+                   "",
+                   true,
+                   DateTime.Now))
+                .RuleFor(c => c.Email, (f, c) =>
+                    f.Internet.Email(c.Nome.ToLower(), c.Sobrenome.ToLower()));
+
+            return cliente;
+        }
+
+
+        public Cliente GerarClienteInvalido()
+        {
+            var genero = new Faker().PickRandom<Name.Gender>();
+            
+            var cliente = new Faker<Cliente>("pt_BR")
+                .CustomInstantiator(f => new Cliente(
+                   Guid.NewGuid(),
+                   f.Name.FirstName(genero),
+                   f.Name.LastName(genero),
+                   f.Date.Past(1, DateTime.Now.AddYears(1)),
+                   "",
+                   true,
+                   DateTime.Now));
+
+            return cliente;
+        }
+
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
