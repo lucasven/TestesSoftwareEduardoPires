26d286c2bcf6f2a27390a73f3992c5cce5bf76e2
lucasven <lucas.venturella@hotmail.com>
2020-03-04 10:29:34 -0300

    implementacao parcial dos mocks

---
+++ Features.Tests/05 - Mock/ClienteServiceTests.cs
@@ -0,0 +1,48 @@
+﻿using Features.Clientes;
+using MediatR;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Features.Tests
+{
+    [Collection(nameof(ClienteBogusCollection))]
+    public class ClienteServiceTests
+    {
+        readonly ClienteTestsBogusFixture _clienteTestsBogusFixture;
+
+        public ClienteServiceTests(ClienteTestsBogusFixture clienteTestsBogusFixture)
+        {
+            _clienteTestsBogusFixture = clienteTestsBogusFixture;
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Sucesso")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_Adicionar_DeveExecutarComSucesso()
+        {
+            // Arrange
+            var cliente = _clienteTestsBogusFixture.GerarClienteValido();
+            var clienteRepo = new Mock<IClienteRepository>();
+            var mediator = new Mock<IMediator>();
+
+            var clienteService = new ClienteService(clienteRepo.Object, mediator.Object);
+
+            // Act
+
+
+            // Assert
+
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Falha")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_Adicionar_DeveFalharDevidoClienteInvalido()
+        {
+
+        }
+
+
+    }
+}
