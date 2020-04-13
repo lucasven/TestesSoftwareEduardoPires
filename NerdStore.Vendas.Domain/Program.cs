7e43660d3d96123e7eb03c3fa33092e83a7934b5
lucas.venturella <lucas.venturella@hammerconsult.com.br>
2020-04-13 11:08:49 -0300

    adicionado nerd store para prática do TDD

---
+++ NerdStore.Vendas.Domain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NerdStore.Vendas.Domain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
